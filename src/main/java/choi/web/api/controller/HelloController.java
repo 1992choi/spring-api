ed33d64e9627bce70386a62c437465ed417f80fc
choi <younggeunn@naver.com>
2023-03-09 22:19:22 +0900

    Swagger 적용

--- src/main/java/choi/web/api/controller/HelloController.java
+++ src/main/java/choi/web/api/controller/HelloController.java
@@ -1,5 +1,7 @@
 package choi.web.api.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,29 +10,34 @@ import java.util.Map;
 @RestController
 public class HelloController {
 
+    @ApiOperation(value = "호출 테스트")
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
 
+    @ApiOperation(value = "GET 메서드 예제", notes = "@PathVariable을 사용한 예제")
     @GetMapping("/hello-get/{name}")
-    public String getMethod(@PathVariable String name) {
+    public String getMethod(@ApiParam(value = "이름") @PathVariable String name) {
         return "Hello GET : " + name;
     }
 
+    @ApiOperation(value = "POST 메서드 예제", notes = "@RequestParam을 사용한 예제")
     @PostMapping("/hello-post")
-    public String postMethod(@RequestParam String name) {
-        return "Hello Post : " + name;
+    public String postMethod(@ApiParam(value = "이름") @RequestParam String name) {
+        return "Hello POST : " + name;
     }
 
+    @ApiOperation(value = "PUT 메서드 예제", notes = "@RequestBody 및 Map을 사용한 예제")
     @PutMapping("/hello-put")
-    public String putMethod(@RequestBody Map<String, String> map) {
-        return "Hello Put : " + map.get("name");
+    public String putMethod(@ApiParam(value = "유저 맵") @RequestBody Map<String, String> map) {
+        return "Hello PUT : " + map.get("name");
     }
 
+    @ApiOperation(value = "DELETE 메서드 예제", notes = "@RequestBody 및 DTO를 사용한 예제")
     @DeleteMapping("/hello-delete")
-    public String deleteMethod(@RequestBody HelloUser helloUser) {
-        return "Hello Delete : " + helloUser.getName();
+    public String deleteMethod(@ApiParam(value = "유저 DTO") @RequestBody HelloUser helloUser) {
+        return "Hello DELETE : " + helloUser.getName();
     }
 
 }
