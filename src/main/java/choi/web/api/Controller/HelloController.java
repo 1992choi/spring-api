b1603668dcdfae355579e549b5878fa70d226758
choi <younggeunn@naver.com>
2023-03-09 21:30:02 +0900

    HTTP 메서드 매핑 예시

--- src/main/java/choi/web/api/Controller/HelloController.java
+++ src/main/java/choi/web/api/Controller/HelloController.java
@@ -1,7 +1,9 @@
 package choi.web.api.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class HelloController {
@@ -11,4 +13,29 @@ public class HelloController {
         return "Hello World";
     }
 
+    @GetMapping("/hello-get/{name}")
+    public String getMethod(@PathVariable String name) {
+        return "Hello GET : " + name;
+    }
+
+    @PostMapping("/hello-post")
+    public String postMethod(@RequestParam String name) {
+        return "Hello Post : " + name;
+    }
+
+    @PutMapping("/hello-put")
+    public String putMethod(@RequestBody Map<String, String> map) {
+        return "Hello Put : " + map.get("name");
+    }
+
+    @DeleteMapping("/hello-delete")
+    public String deleteMethod(@RequestBody HelloUser helloUser) {
+        return "Hello Delete : " + helloUser.getName();
+    }
+
+}
+
+@Data
+class HelloUser {
+    String name;
 }
