1f32e15613bc2664fa0a46d7c0e4c8c117fab53d
choi <younggeunn@naver.com>
2023-03-10 19:26:08 +0900

    컨트롤러명 변경

--- src/main/java/choi/web/api/controller/SampleController.java
+++ src/main/java/choi/web/api/controller/SampleController.java
@@ -8,41 +8,36 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-public class HelloController {
+public class SampleController {
 
-    @ApiOperation(value = "호출 테스트")
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
-    }
-
+    /* HTTP Request Mapping 및 Swagger 샘플 */
     @ApiOperation(value = "GET 메서드 예제", notes = "@PathVariable을 사용한 예제")
-    @GetMapping("/hello-get/{name}")
+    @GetMapping("/sample/get/{name}")
     public String getMethod(@ApiParam(value = "이름") @PathVariable String name) {
-        return "Hello GET : " + name;
+        return "GET : " + name;
     }
 
     @ApiOperation(value = "POST 메서드 예제", notes = "@RequestParam을 사용한 예제")
-    @PostMapping("/hello-post")
+    @PostMapping("/sample/post")
     public String postMethod(@ApiParam(value = "이름") @RequestParam String name) {
-        return "Hello POST : " + name;
+        return "POST : " + name;
     }
 
     @ApiOperation(value = "PUT 메서드 예제", notes = "@RequestBody 및 Map을 사용한 예제")
-    @PutMapping("/hello-put")
+    @PutMapping("/sample/put")
     public String putMethod(@ApiParam(value = "유저 맵") @RequestBody Map<String, String> map) {
-        return "Hello PUT : " + map.get("name");
+        return "PUT : " + map.get("name");
     }
 
     @ApiOperation(value = "DELETE 메서드 예제", notes = "@RequestBody 및 DTO를 사용한 예제")
-    @DeleteMapping("/hello-delete")
-    public String deleteMethod(@ApiParam(value = "유저 DTO") @RequestBody HelloUser helloUser) {
-        return "Hello DELETE : " + helloUser.getName();
+    @DeleteMapping("/sample/delete")
+    public String deleteMethod(@ApiParam(value = "유저 DTO") @RequestBody SampleUser sampleUser) {
+        return "DELETE : " + sampleUser.getName();
     }
 
 }
 
 @Data
-class HelloUser {
+class SampleUser {
     String name;
 }
