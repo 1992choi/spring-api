ebd0920242af3a38a6aa22c17a1b8986fdf03308
choi <younggeunn@naver.com>
2023-03-10 19:20:44 +0900

    스프링부트 버전변경 및 버전변경에 따른 Swagger 설정변경

--- src/main/java/choi/web/api/config/SwaggerConfig.java
+++ src/main/java/choi/web/api/config/SwaggerConfig.java
@@ -8,11 +8,11 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
+    // http://localhost:8080/swagger-ui/index.html
 
     @Bean
     public Docket api() {
