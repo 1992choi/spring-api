0d938fc1f7999a4c603783f4dd8d49ec9fa28b87
choi <younggeunn@naver.com>
2023-03-14 20:26:55 +0900

    REST Docs 커스터마이징 - 응답 JSON 정렬

--- src/test/java/choi/web/api/controller/MemberControllerTest.java
+++ src/test/java/choi/web/api/controller/MemberControllerTest.java
@@ -1,11 +1,13 @@
 package choi.web.api.controller;
 
+import choi.web.api.config.RestDocsConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
+@Import(RestDocsConfiguration.class)
 class MemberControllerTest {
 
     @Autowired
