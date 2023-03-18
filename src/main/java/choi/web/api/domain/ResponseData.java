cf0523d38492e12d46230d2c0d242d670c5a2890
choi <younggeunn@naver.com>
2023-03-18 12:57:40 +0900

    공통응답에서 NULL 제외

--- src/main/java/choi/web/api/domain/ResponseData.java
+++ src/main/java/choi/web/api/domain/ResponseData.java
@@ -1,10 +1,12 @@
 package choi.web.api.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Builder
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL) // Null 값인 필드 제외
 public class ResponseData {
 
     String resultCode;
