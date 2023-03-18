b11c07f4127e0ace3e7515587a120aa11ca23567
choi <younggeunn@naver.com>
2023-03-18 12:55:20 +0900

    @ExceptionHandler(NoHandlerFoundException.class) 추가 (404 응답처리 목적)

--- src/main/java/choi/web/api/exception/ExceptHandler.java
+++ src/main/java/choi/web/api/exception/ExceptHandler.java
@@ -7,10 +7,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    protected ResponseEntity<ResponseData> noHandlerFoundException(NoHandlerFoundException e) {
+        log.info("ExceptHandler.noHandlerFoundException = {}", e.getMessage());
+        return ResponseEntity.ok(
+                ResponseData.builder()
+                        .resultCode("9999")
+                        .resultMessage("URL을 확인해주세요.")
+                        .build()
+        );
+    }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
