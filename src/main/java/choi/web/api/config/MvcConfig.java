7fc7ba30e0966594fed06b68af70ad85bb00d2c8
choi <younggeunn@naver.com>
2023-07-21 22:03:51 +0900

    트래픽제한을 위해 Bucket4j 적용

--- src/main/java/choi/web/api/config/MvcConfig.java
+++ src/main/java/choi/web/api/config/MvcConfig.java
@@ -1,6 +1,8 @@
 package choi.web.api.config;
 
+import choi.web.api.interceptor.TrafficInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +13,12 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/docs/**")
                 .addResourceLocations("classpath:/static/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        TrafficInterceptor trafficInterceptor = new TrafficInterceptor();
+        registry.addInterceptor(trafficInterceptor);
     }
 
 }
