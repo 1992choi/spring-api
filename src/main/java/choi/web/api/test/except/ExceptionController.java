1fa17741fbd7359ba1bb9bf77d32867f5aece227
choi <younggeunn@naver.com>
2023-09-18 21:37:41 +0900

    Checked Exception과 Unchecked Exception의 종속성

--- src/main/java/choi/web/api/test/except/ExceptionController.java
+++ src/main/java/choi/web/api/test/except/ExceptionController.java
@@ -7,14 +7,32 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.SQLException;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ExceptionController {
 
+    private final ExceptionService exceptionService;
+
     @GetMapping("/test/exception")
     public ResponseEntity<ResponseData> testException() {
         int i = 10 / 0;
+        return ResponseEntity.ok(new ResponseData("0000", "성공하였습니다."));
+    }
+
+    // Checked Exception - 예외로 인해 종속 발생
+    @GetMapping("/test/checked-exception")
+    public ResponseEntity<ResponseData> chekced() throws SQLException {
+        exceptionService.findAllWithChecked();
+        return ResponseEntity.ok(new ResponseData("0000", "성공하였습니다."));
+    }
+
+    // Unchecked Exception - Checked Exception을 Unchecked Exception으로 변경하여 종속 제거
+    @GetMapping("/test/unchecked-exception")
+    public ResponseEntity<ResponseData> unchekced() {
+        exceptionService.findAllWithUnChecked();
         return ResponseEntity.ok(new ResponseData("0000", "성공하였습니다."));
     }
 
---
+++ src/main/java/choi/web/api/test/except/ExceptionRepository.java
@@ -0,0 +1,40 @@
+package choi.web.api.test.except;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ExceptionRepository {
+
+    // Checked Exception - 예외로 인해 종속 발생
+    public void findAllWithChecked() throws SQLException {
+        runSQL();
+    }
+
+    // Unchecked Exception - Checked Exception을 Unchecked Exception으로 변경하여 종속 제거
+    public void findAllWithUnChecked() {
+        try {
+            runSQL();
+        } catch (SQLException e) {
+            throw new SqlRuntimeException(e); // Checked Exception -> Unchecked Exception 전환
+        }
+    }
+
+    private void runSQL() throws SQLException {
+        throw new SQLException();
+    }
+
+}
+
+class SqlRuntimeException extends RuntimeException {
+
+    public SqlRuntimeException(Throwable cause) {
+        super(cause);
+    }
+
+}
