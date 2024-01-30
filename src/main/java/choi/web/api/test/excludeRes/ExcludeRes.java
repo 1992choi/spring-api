f63d46a1ec0b2b676367fc7c004453049aeb9d81
choi <younggeunn@naver.com>
2024-01-30 20:29:26 +0900

    응답값 테스트 코드

--- src/main/java/choi/web/api/test/excludeRes/ExcludeRes.java
+++ src/main/java/choi/web/api/test/excludeRes/ExcludeRes.java
@@ -5,20 +5,24 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import jakarta.persistence.Transient;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.List;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class ExcludeRes {
 
-    @JsonIgnore // 응답값에 포함하지 않음
-    List<String> res1;
+    String res1;
 
-    @Transient // 영속 대상에서 제외할 때 사용하는 어노테이션
-    List<String> res2;
+    @JsonIgnore // 응답값에 포함하지 않음
+    String res2;
 
     @JsonInclude(JsonInclude.Include.NON_NULL) // 값이 NULL일 때만 응답값에 포함하지 않음
-    List<String> res3;
+    String res3;
+
+    @Transient // 영속 대상에서 제외할 때 사용하는 어노테이션
+    String res4;
+
+    transient String res5; // 직렬화 제외
 
 }
--- src/main/java/choi/web/api/test/excludeRes/ExcludeResController.java
+++ src/main/java/choi/web/api/test/excludeRes/ExcludeResController.java
@@ -1,10 +1,13 @@
 package choi.web.api.test.excludeRes;
 
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 
+@Slf4j
 @RestController
 public class ExcludeResController {
 
@@ -14,15 +17,22 @@ public class ExcludeResController {
     @GetMapping("/test/exclude-res/exist")
     public ExcludeRes existValue() {
         ExcludeRes excludeRes = new ExcludeRes();
-        excludeRes.setRes1(new ArrayList<>());
-        excludeRes.setRes2(new ArrayList<>());
-        excludeRes.setRes3(new ArrayList<>());
+        excludeRes.setRes1("res1");
+        excludeRes.setRes2("res2");
+        excludeRes.setRes3("res3");
+        excludeRes.setRes4("res4");
+        excludeRes.setRes5("res5");
+
+        Gson gson = new Gson();
+        log.info("gson = {}", gson.toJson(excludeRes)); // gson = {"res1":"res1","res2":"res2","res3":"res3","res4":"res4"}
 
         return excludeRes;
         /* 응답값
             {
-                "res2": [],
-                "res3": []
+                "res1": "res1",
+                "res3": "res3",
+                "res4": "res4",
+                "res5": "res5"
             }
         */
     }
@@ -32,10 +42,17 @@ public class ExcludeResController {
      */
     @GetMapping("/test/exclude-res/not-exist")
     public ExcludeRes notExistValue() {
-        return new ExcludeRes();
+        ExcludeRes excludeRes = new ExcludeRes();
+
+        Gson gson = new Gson();
+        log.info("gson = {}", gson.toJson(excludeRes)); // gson = {}
+
+        return excludeRes;
         /* 응답값
             {
-                "res2": null
+                "res1": null,
+                "res4": null,
+                "res5": null
             }
         */
     }
